aaf105aecde8d09bb7d0cdc32af34897bc6d2b0b
qupc <qupengcheng4514348@qq.com>
2019-12-04 16:51:08 +0800

    first commit

---
+++ UdfsDevTools.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+import base64
+import hashlib
+import hmac
+import os
+import time
+from hashlib import sha1
+
+class UdfsDevTools():
+
+    def GetFileMd5(self,filePath):
+        if not os.path.isfile(filePath):
+            return
+        myhash = hashlib.md5()
+        f = open(filePath, 'rb')
+        while True:
+            b = f.read(8096)
+            if not b:
+                break
+            myhash.update(b)
+        f.close()
+        return myhash.hexdigest()
+
+    #expireSec:指定毫秒后过期时间
+    #secretKey:秘钥,用于udfs访问,从udfs官方获取
+    #uosAccount:uos账号
+    #filePath:需要上传的文件路径
+    #udfsCallbackUrl:回调url,udfs上传完毕后回调该url,将返回（文件id，哈希值，文件size），该参数可为空
+    def getUploadUrl(self,expireSec, secretKey, uosAccount, filePath, udfsCallbackUrl):
+        fileName = os.path.basename(filePath);
+        fileSize = str(os.path.getsize(filePath))
+        md5 = self.GetFileMd5(filePath);
+        expireTime = int(time.time())
+        expireTime += expireSec;
+        expireTime=str(expireTime)
+        if udfsCallbackUrl == None or udfsCallbackUrl == "":
+            json = "{\"ver\":0,\"expired\":" + expireTime + ",\"callback_url\":\"\",\"callback_body\":\"\",\"ext\":{\"file_name\":\"" + fileName + "\",\"size\":" + fileSize + ",\"md5\":\"" + md5 + "\"}}";
+        else:
+            json = "{\"ver\":0,\"expired\":" + expireTime + ",\"callback_url\":\"" + udfsCallbackUrl + "\",\"callback_body\":\"{\\\"file_size\\\":\\\"$(size)\\\",\\\"hash\\\":\\\"$(hash)\\\",\\\"file_id\\\":0}\",\"ext\":{\"file_name\":\"" + fileName + "\",\"size\":" + fileSize + ",\"md5\":\"" + md5 + "\"}}";
+        try :
+            textByte = json.encode(encoding="utf-8")
+            encodedPolicy = base64.urlsafe_b64encode(textByte)
+            my_sign = hmac.new(secretKey.encode(encoding="utf-8"), encodedPolicy, sha1).digest()
+            encodeSign = base64.b64encode(my_sign)
+        except Exception as ex:
+            raise Exception("构建下载链接出错" + ex.toString())
+        token = uosAccount + ":" + str(encodeSign, encoding="utf-8") + ":" + str(encodedPolicy, encoding="utf-8")
+        downloadUrl = 'http://api.udfs.one:15001/api/v0/add' + "?token=" + token
+        return downloadUrl;
+
+    #uosAccount:uos账号
+    #secretKey:秘钥,用于udfs访问,从udfs官方获取
+    #expireSec:指定毫秒后过期时间
+    #hashvalue:要下载的对象哈希值
+    def getDownloadUrl(self,uosAccount,secretKey,expireSec,hashvalue):
+        expireTime = int(time.time())
+        expireTime += expireSec;
+        json = "{\"ver\":0,\"expired\": "+str(expireTime)+",\"ext\":{}}"
+        try :
+            textByte = json.encode(encoding="utf-8")
+            encodedPolicy = base64.urlsafe_b64encode(textByte)
+            my_sign = hmac.new(secretKey.encode(encoding="utf-8"), encodedPolicy, sha1).digest()
+            encodeSign = base64.b64encode(my_sign)
+        except Exception as ex:
+            raise Exception("构建下载链接出错" + ex.toString())
+        token = uosAccount + ":" + str(encodeSign, encoding="utf-8") + ":" + str(encodedPolicy, encoding="utf-8")
+        downloadUrl = 'http://api.udfs.one:15001/api/v0/get/' + hashvalue + "?token=" + token
+        return downloadUrl
+
+
+
+
---
+++ example.py
@@ -0,0 +1,18 @@
+import UdfsDevTools
+import requests
+
+def download():
+    tool = UdfsDevTools.UdfsDevTools()
+    url = tool.getDownloadUrl("uosAccount","secretKey", 10000,"QmPuzPAtLjRpJNHM5qbuShS2s1Etki92C3672JLt8oJiGg")
+    conn = requests.get(url)
+
+    with open('QmPuzPAtLjRpJNHM5qbuShS2s1Etki92C3672JLt8oJiGg', "wb") as code:
+        code.write(conn.content)
+def upload():
+    files = {'file': open('D:\\home\\1.mp4','rb')}
+    tool = UdfsDevTools.UdfsDevTools()
+    url = tool.getUploadUrl(10000,"secretKey","uosAccount","D:\\home\\1.mp4","")
+    upload_res = requests.post(url,files=files)
+    print(upload_res)
+upload()
+download()
