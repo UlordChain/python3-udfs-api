fc03f7335bd7845186fe70311d82469b07c9cd19
qupc <qupengcheng4514348@qq.com>
2019-12-05 11:47:45 +0800

    add delete method

--- UdfsDevTools.py
+++ UdfsDevTools.py
@@ -26,7 +26,7 @@ class UdfsDevTools():
     #uosAccount:uos账号
     #filePath:需要上传的文件路径
     #udfsCallbackUrl:回调url,udfs上传完毕后回调该url,将返回（文件id，哈希值，文件size），该参数可为空
-    def getUploadUrl(self,expireSec, secretKey, uosAccount, filePath, udfsCallbackUrl):
+    def getUploadUrl(self,expireSec, uosAccount,secretKey,  filePath, udfsCallbackUrl):
         fileName = os.path.basename(filePath);
         fileSize = str(os.path.getsize(filePath))
         md5 = self.GetFileMd5(filePath);
@@ -43,10 +43,10 @@ class UdfsDevTools():
             my_sign = hmac.new(secretKey.encode(encoding="utf-8"), encodedPolicy, sha1).digest()
             encodeSign = base64.urlsafe_b64encode(my_sign)
         except Exception as ex:
-            raise Exception("构建下载链接出错" + ex.toString())
+            raise Exception("构建上传链接出错" + ex.toString())
         token = uosAccount + ":" + str(encodeSign, encoding="utf-8") + ":" + str(encodedPolicy, encoding="utf-8")
-        downloadUrl = 'http://api.udfs.one:15001/api/v0/add' + "?token=" + token
-        return downloadUrl;
+        uploadUrl = 'http://api.udfs.one:15001/api/v0/add' + "?token=" + token
+        return uploadUrl;
 
     #uosAccount:uos账号
     #secretKey:秘钥,用于udfs访问,从udfs官方获取
@@ -64,8 +64,23 @@ class UdfsDevTools():
         except Exception as ex:
             raise Exception("构建下载链接出错" + ex.toString())
         token = uosAccount + ":" + str(encodeSign, encoding="utf-8") + ":" + str(encodedPolicy, encoding="utf-8")
-        downloadUrl = 'http://api.udfs.one:15001/api/v0/get/' + hashvalue + "?token=" + token
+        downloadUrl = 'http://api.udfs.one:15001/api/v0/cat/' + hashvalue + "?token=" + token
         return downloadUrl
+
+    def deletefile(self,uosAccount,secretKey,expireSec,hashvalue):
+        expireTime = int(time.time())
+        expireTime += expireSec;
+        json = "{\"ver\":0,\"expired\": "+str(expireTime)+",\"ext\":{}}"
+        try :
+            textByte = json.encode(encoding="utf-8")
+            encodedPolicy = base64.urlsafe_b64encode(textByte)
+            my_sign = hmac.new(secretKey.encode(encoding="utf-8"), encodedPolicy, sha1).digest()
+            encodeSign = base64.urlsafe_b64encode(my_sign)
+        except Exception as ex:
+            raise Exception("构建删除链接出错" + ex.toString())
+        token = uosAccount + ":" + str(encodeSign, encoding="utf-8") + ":" + str(encodedPolicy, encoding="utf-8")
+        deleteUrl = 'http://api.udfs.one:15001/api/v0/delete/' + hashvalue + "?token=" + token
+        return deleteUrl
 
 
 
--- example.py
+++ example.py
@@ -1,18 +1,26 @@
 import UdfsDevTools
 import requests
 
+def delete():
+    tool = UdfsDevTools.UdfsDevTools()
+    url = tool.deletefile("uosAccount","secretKey", 10000,"QmTLXPs77vn6ss268ghAUbnMFR12yzR5cHr3kud19SZWTs")
+    conn = requests.delete(url)
+    print(conn)
+
 def download():
     tool = UdfsDevTools.UdfsDevTools()
-    url = tool.getDownloadUrl("uosAccount","secretKey", 10000,"QmPuzPAtLjRpJNHM5qbuShS2s1Etki92C3672JLt8oJiGg")
+    url = tool.getDownloadUrl("uosAccount","secretKey", 10000,"QmTLXPs77vn6ss268ghAUbnMFR12yzR5cHr3kud19SZWTs")
     conn = requests.get(url)
 
-    with open('QmPuzPAtLjRpJNHM5qbuShS2s1Etki92C3672JLt8oJiGg', "wb") as code:
+    with open('QmTLXPs77vn6ss268ghAUbnMFR12yzR5cHr3kud19SZWTs', "wb") as code:
         code.write(conn.content)
 def upload():
     files = {'file': open('D:\\home\\1.mp4','rb')}
     tool = UdfsDevTools.UdfsDevTools()
-    url = tool.getUploadUrl(10000,"secretKey","uosAccount","D:\\home\\1.mp4","")
+    url = tool.getUploadUrl(10000,"uosAccount","secretKey","D:\\home\\1.mp4","")
     upload_res = requests.post(url,files=files)
     print(upload_res)
-# upload()
+upload()
+download()
+delete()
 download()
